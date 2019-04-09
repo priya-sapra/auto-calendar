c07d6487bf0e85f9dcb605068a66e14e5baf5b32
saprap1 <priyasapra.2017@gmail.com>
2019-04-09 13:58:06 -0400

    Displays user input

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,49 +1,48 @@
 <template>
-	<div>
-		<p>Class 1</p>
+  <div>
+    <p>Class 1</p>
+      <input 
+        type="text" 
+        v-model="message">
+      <p>Class 2</p>
+      <input 
+        type="text" 
+        v-model="message1">
+      <p>Class 3</p>
+      <input 
+        type="text" 
+        v-model="message2">
+      <p>Class 4</p>
 		<input 
-			type="text" 
-			v-model="message">
-		<p>Class 2</p>
-		<input 
-			type="text" 
-			v-model="message1">
-		<p>Class 3</p>
-		<input 
-			type="text" 
-			v-model="message2">
-		<p>Class 4</p>
-		<input 
-			type="text" 
-			v-model="message3">
-		<p>Classes: {{ message }}, {{ message1 }}, {{ message2 }}, {{ message3 }}</p>
-		<br>
-		<select v-model="selected">
-			<option disabled value="">Please select one</option>
-			<option>Spring</option>
-			<option>Fall</option>
-		</select>
-		<br>
-		<br>
-		<span>Selected: {{ selected }}</span>
-	</div>
+          type="text" 
+          v-model="message3">
+      <p>Classes: {{ message }}, {{ message1 }}, {{ message2 }}, {{ message3 }}</p>
+      <br>
+      <select v-model="selected">
+      <option disabled value="">Please select one</option>
+      <option>Spring</option>
+      <option>Fall</option>
+      </select>
+      <br>
+      <br>
+    <span>Selected: {{ selected }}</span>
+  </div>
 </template>
 
 <script>
+import * as mdc from "material-components-web";
 export default {
-	data() {
-		return{
-			message: '',
-			message1: '',
-			message2: '',
-			message3: '',
-			selected: ''
-		}
-	}
+  data() {
+    return {
+      message: "",
+      message1: "",
+      message2: "",
+      message3: "",
+      selected: ""
+    };
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
