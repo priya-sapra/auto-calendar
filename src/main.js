07d16d9df6c4f667490b3c235f875cba2b6291f4
saprap1 <priyasapra.2017@gmail.com>
2019-04-16 00:55:48 -0400

    database changes

--- src/main.js
+++ src/main.js
@@ -4,31 +4,7 @@ import store from "./store";
 import router from "./router";
 import UserForm from "./components/UserForm.vue";
 
-import firebase from "firebase";
-// Required for side-effects
-require("firebase/firestore");
 
-var config = {
-  apiKey: "AIzaSyC-F4u5SEsl5EYRnG6nfGPKz5a8iFTQInM",
-  authDomain: "auto-calendar-a48c3.firebaseapp.com",
-  databaseURL: "https://auto-calendar-a48c3.firebaseio.com",
-  projectId: "auto-calendar-a48c3",
-  storageBucket: "auto-calendar-a48c3.appspot.com",
-  messagingSenderId: "116933816937"
-};
-firebase.initializeApp(config);
-
-// Initalize Cloud Firestore through Firebase
-var db = firebase.firestore();
-
-// Make the DB is accessible from anywhere
-window.db = db;
-
-// IDK IF I ACTUALLY NEED THIS BC IT'S NO LONGER IN BETA BUT...
-// Disable deprecated features
-db.settings({
-  timestampsInSnapshots: true
-});
 
 Vue.config.productionTip = false;
 
