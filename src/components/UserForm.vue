088d96c0b5047634e090c1ed536c76d165690607
saprap1 <priyasapra.2017@gmail.com>
2019-05-15 09:07:34 -0400

    write classes to database

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -1,62 +1,88 @@
 <template>
+
   <div class="container">
-    <form action="action_page.php">
-      <label for="c1">Class 1</label>
-      <input
-        @keyup.enter="saveMessage1"
-        type="text"
-        id="c1"
-        name="class1"
-        placeholder="course name"
-        v-model="message"
-      />
-      <label for="c2">Class 2</label>
-      <input
-        type="text"
-        id="c2"
-        name="class2"
-        placeholder="course name"
-        v-model="message1"
-      />
-      <label for="c3">Class 3</label>
-      <input
-        type="text"
-        id="c3"
-        name="class3"
-        placeholder="course name"
-        v-model="message2"
-      />
-      <label for="c4">Class 4</label>
-      <input
-        type="text"
-        id="c4"
-        name="class4"
-        placeholder="course name"
-        v-model="message3"
-      />
-      <p>
-        Classes: {{ message }}, {{ message1 }}, {{ message2 }}, {{ message3 }}
-      </p>
-      <br />
-      <label for="sem">Choose a semester</label>
-      <select v-model="selected">
-        <option disabled value="">Please select a semester</option>
-        <option>Spring</option>
-        <option>Fall</option>
-      </select>
-      <br />
-      <br />
-      <span>Selected: {{ selected }}</span>
-      <br />
-      <br />
-      <input type="submit" value="Submit" />
-    </form>
+    <label for="c1">Class 1</label>
+    Monday: <input type="checkbox" name="mon" />
+    Tuesday: <input type="checkbox" name="tue" /> 
+    Wednesday: <input type="checkbox" name="wed" /> 
+    Thursday: <input type="checkbox" name="th" /> 
+    Friday: <input type="checkbox" name="fri" />
+    <input
+      @keyup.enter="saveMessage1"
+      type="text"
+      id="c1"
+      name="class1"
+      placeholder="course name"
+      v-model="message"
+    />
+    <label for="c2">Class 2</label>
+    Monday: <input type="checkbox" name="mon" />
+    Tuesday: <input type="checkbox" name="tue" /> 
+    Wednesday: <input type="checkbox" name="wed" /> 
+    Thursday: <input type="checkbox" name="th" /> 
+    Friday: <input type="checkbox" name="fri" />
+    <input
+      @keyup.enter="saveMessage2"
+      type="text"
+      id="c2"
+      name="class2"
+      placeholder="course name"
+      v-model="message1"
+    />
+    <label for="c3">Class 3</label>
+    Monday: <input type="checkbox" name="mon" />
+    Tuesday: <input type="checkbox" name="tue" /> 
+    Wednesday: <input type="checkbox" name="wed" /> 
+    Thursday: <input type="checkbox" name="th" /> 
+    Friday: <input type="checkbox" name="fri" />
+    <input
+      @keyup.enter="saveMessage3"
+      type="text"
+      id="c3"
+      name="class3"
+      placeholder="course name"
+      v-model="message2"
+    />
+    <label for="c4">Class 4</label>
+    Monday: <input type="checkbox" name="mon" />
+    Tuesday: <input type="checkbox" name="tue" /> 
+    Wednesday: <input type="checkbox" name="wed" /> 
+    Thursday: <input type="checkbox" name="th" /> 
+    Friday: <input type="checkbox" name="fri" />
+    <input
+      @keyup.enter="saveMessage4"
+      type="text"
+      id="c4"
+      name="class4"
+      placeholder="course name"
+      v-model="message3"
+    />
+    <p>
+      Classes: {{ message }}, {{ message1 }}, {{ message2 }}, {{ message3 }}
+    </p>
+    <br />
+    <label for="sem">Choose a semester</label>
+    <select v-model="selected">
+      <option disabled value="">Please select a semester</option>
+      <option>Spring</option>
+      <option>Fall</option>
+    </select>
+    <br />
+    <br />
+    <span>Selected: {{ selected }}</span>
+    <br />
+    <br />
+    <!--
+    Here, I'm trying to submit everything to the database just by clicking the button
+    <input type="submit" value="Submit" @onclick="saveMessages"/>
+    -->
   </div>
 </template>
 
 <script>
+
 //import * as mdc from "material-components-web";
-import firebase from "firebase";
+//import firebase from "firebase";
 import { db } from "../main.js";
 
 //var db = firebase.firestore();
@@ -75,7 +101,25 @@ export default {
     saveMessage1() {
       // Save message1 to firestore
       db.collection("class").add({
-        c1: this.message1
+        c1: this.message
+      });
+    },
+    saveMessage2() {
+      // Save message1 to firestore
+      db.collection("class").add({
+        c2: this.message1
+      });
+    },
+    saveMessage3() {
+      // Save message1 to firestore
+      db.collection("class").add({
+        c3: this.message2
+      });
+    },
+    saveMessage4() {
+      // Save message1 to firestore
+      db.collection("class").add({
+        c4: this.message3
       });
     }
   }
@@ -88,13 +132,15 @@ export default {
   background-color: #f2f2f2;
   padding: 20px;
 }
-.label {
-  color: #18b1ff;
+label {
   text-align: left;
+  color: #18b1ff;
+}
+p {
+  font-weight: bold;
 }
 input[type="text"],
-select,
-textarea {
+select {
   width: 100%; /* Full width */
   padding: 12px; /* Some padding */
   border: 1px solid #ccc; /* Gray border */
@@ -102,6 +148,5 @@ textarea {
   box-sizing: border-box; /* Make sure that padding and width stays in place */
   margin-top: 6px; /* Add a top margin */
   margin-bottom: 16px; /* Bottom margin */
-  resize: vertical; /* Allow the user to vertically resize the textarea (not horizontally) */
 }
 </style>
